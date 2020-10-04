48f9ff67be17e8a90a882f7a3447a0b323448cb9
Amit Grover <amitgrover07@rediffmail.com>
2020-10-04 14:08:32 +0530

    comment added for main method

--- src/javaphase1/FileManager.java
+++ src/javaphase1/FileManager.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class FileManager {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) //main method
+	{
 		System.out.println("\nWelcome to File Management System using Java, Developed by Amit Grover");
 
 		Scanner in = new Scanner(System.in);
